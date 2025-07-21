54a93f887aa40df16fbc36616d24f9039f473681
Stephen <amungai37@gmail.com>
2025-07-21 12:27:33 +0300

    Finally

--- sevenflags/sevenflags/core/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import ExchangeRate
-
-def available_currencies(request):
-    currencies = ExchangeRate.objects.values_list("target_currency", flat=True)
-    return {
-        "available_currencies": ["KES"] + list(currencies)  # Always include KES as base
-    }
